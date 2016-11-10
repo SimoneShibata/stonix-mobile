f68e0621e6f735a8821a1ddcafe7883e595a3326
Augusto Zeponi <augustozeponi@hotmail.com>
2016-11-10 09:02:38 -0200

    #8969 [mobile] upload de imagem

--- www/js/controllers/LoginCtrl.js
+++ www/js/controllers/LoginCtrl.js
@@ -1,4 +1,4 @@
-app.controller('LoginCtrl', function ($scope, $rootScope, $http, $state, $filter, $ionicPopup, $timeout, $ionicHistory, $cordovaCamera) {
+app.controller('LoginCtrl', function ($scope, $rootScope, $http, $state, $filter, $ionicPopup, $timeout, $ionicHistory, $cordovaCamera, $cordovaImagePicker) {
 
 // Cadastrar - register
   $scope.fotoPerfilBase64 = "";
@@ -44,7 +44,7 @@ app.controller('LoginCtrl', function ($scope, $rootScope, $http, $state, $filter
     $scope.login();
   };
 
-  $scope.tirarFoto = function(){
+  $scope.tirarFoto = function () {
     var options = {
       quality: 50,
       destinationType: Camera.DestinationType.DATA_URL,
@@ -52,17 +52,43 @@ app.controller('LoginCtrl', function ($scope, $rootScope, $http, $state, $filter
       allowEdit: true,
       encodingType: Camera.EncodingType.JPEG,
       targetWidth: 500,
-      targetHeight:500,
+      targetHeight: 500,
       saveToPhotoAlbum: false,
-      correctOrientation:true
+      correctOrientation: true
     };
-    $cordovaCamera.getPicture(options).then(function(imageData) {
+    $cordovaCamera.getPicture(options).then(function (imageData) {
       var imageDataCamera = "data:image/jpeg;base64," + imageData;
       $scope.fotoPerfilBase64 = imageDataCamera;
       cod = imageData;
-    }, function(err) {
+    }, function (err) {
       // error
     });
   }
+
+  $scope.abrirGaleria = function () {
+    // Image picker will load images according to these settings
+    var options = {
+      maximumImagesCount: 1, // Max number of selected images, I'm using only one for this example
+      width: 800,
+      height: 800,
+      quality: 80
+
+    };
+
+    $cordovaImagePicker.getPictures(options).then(function (results) {
+      // Loop through acquired images
+      for (var i = 0; i < results.length; i++) {
+        var imageDataCamera = results[i]+"";
+
+        cod = results[i];
+
+        //$scope.fotoPerfilBase64 = imageDataCamera;
+
+        $scope.fotoPerfilBase64  = imageDataCamera;
+      }
+    }, function (error) {
+      console.log('Error: ' + JSON.stringify(error));    // In case of error
+    });
+  };
 });
 
